bc13d53d6842d79ea69bfb5d5d034cac7f2479b5
moonk1212 <zm8208@naver.com>
2020-03-30 18:40:58 +0900

    ,

--- js/maskcrawl5.js
+++ js/maskcrawl5.js
@@ -1,18 +1,14 @@
 
 var client = require('cheerio-httpcli'); 
-var URL =require('url');
 let url = 'https://smartstore.naver.com/dkpharm_naturesvitamin/category/ebc4bcef2a0f46bca28e03e81d0d1cfa?cp=1';
 var param = {}; 
 client.fetch(url, param, function(err, $, res){
   if(err){
        console.log(err);
-        return; 
-     }
+        return; }
  $(".area_status").each(function(idx) { 
-   
     result=$("span.blind").text();
      }); 
-
      sold="일시품절";
      if(result.includes(sold)==true){
 
